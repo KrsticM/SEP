abe944c66f94f8c0ced5ef4d76e307f304820a43
Kristina G <pinksugarpill@gmail.com>
2019-11-24 10:18:11 +0100

    Registration and login local state and required fields validation handling

--- payme-client/src/containers/Login/index.jsx
+++ payme-client/src/containers/Login/index.jsx
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Link from '@material-ui/core/Link';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
 function Login() {
+  const [state, setState] = useState({
+    email: '',
+    password: ''
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setState({
+      ...state,
+      [name]: value
+    });
+    if (errors[name]) {
+      setErrors({
+        ...errors,
+        [name]: undefined
+      });
+    }
+  };
+
+  const validateForm = () => {
+    const validationErrors = {};
+    let isValid = true;
+    if (!state.email) {
+      validationErrors.email = "Email required";
+      isValid = false;
+    }
+    if (!state.password) {
+      validationErrors.password = "Password required";
+      isValid = false;
+    }
+    setErrors(validationErrors);
+    return isValid;
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    if (!validateForm()) {
+      return;
+    }
+    // handle login afterwards
+  };
+
   return (
     <div className="signup__container">
       <Link href="/">
@@ -15,14 +59,26 @@ function Login() {
       <form className="form__container">
         <TextField
           label="Email"
+          name="email"
+          value={state.email}
+          onChange={handleChange}
+          error={errors.email}
           required
         />
         <TextField
           label="Password"
+          name="password"
+          value={state.password}
+          onChange={handleChange}
+          error={errors.password}
           required
         />
         <div className="signup__actions">
-          <Button variant="contained" color="primary">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleLogin}
+          >
             Login
           </Button>
           <Link href="/signup">
--- payme-client/src/containers/Signup/index.jsx
+++ payme-client/src/containers/Signup/index.jsx
@@ -1,10 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from '@material-ui/core/Link';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import './Signup.css';
 
+const AUTHORITY = {
+  PERSONAL: 'PERSONAL',
+  ENTERPRISE: 'ENTERPRISE'
+};
+
+const requiredFields = [
+  {
+    value: 'email',
+    label: 'Email'
+  },
+  {
+    value: 'firstName',
+    label: 'First name'
+  },
+  {
+    value: 'lastName',
+    label: 'Last name'
+  },
+  {
+    value: 'password',
+    label: 'Password'
+  },
+  {
+    value: 'website',
+    label: 'Website'
+  }
+];
+
 function Signup() {
+  const [state, setState] = useState({
+    firstName: '',
+    lastName: '',
+    organizationName: '',
+    email: '',
+    password: '',
+    website: '',
+    authority: AUTHORITY.PERSONAL
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setState({
+      ...state,
+      [name]: value
+    });
+    if (errors[name]) {
+      setErrors({
+        ...errors,
+        [name]: undefined
+      });
+    }
+  };
+
+
+  const validateForm = () => {
+    const validationErrors = {};
+    let isValid = true;
+    requiredFields.forEach((field) => {
+      if (!state[field.value]) {
+        validationErrors[field.value] = `${[field.label]} required`;
+        isValid = false;
+      }
+    });
+    setErrors(validationErrors);
+    return isValid;
+  };
+
+  const handleRegistration = (event) => {
+    event.preventDefault();
+    if (!validateForm()) {
+      return;
+    }
+    // handle registration afterwards
+  };
+
   return (
     <div className="signup__container">
       <Link href="/">
@@ -14,27 +90,31 @@ function Signup() {
         />
       </Link>
       <form className="form__container">
-        <TextField
-          label="First name"
-          required
-        />
-        <TextField
-          label="Last name"
-          required
-        />
+        {
+          requiredFields.map((field) => (
+            <TextField
+              key={field.value}
+              label={field.label}
+              name={field.value}
+              value={state[field.value]}
+              error={errors[field.value]}
+              onChange={handleChange}
+              required
+            />
+          ))
+        }
         <TextField
           label="Organization name"
-        />
-        <TextField
-          label="Email"
-          required
-        />
-        <TextField
-          label="Website"
-          required
+          name="organizationName"
+          value={state.organizationName}
+          onChange={handleChange}
         />
         <div className="signup__actions">
-          <Button variant="contained" color="primary">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleRegistration}
+          >
             Register
           </Button>
           <Link href="/login">
